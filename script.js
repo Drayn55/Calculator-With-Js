654cbfc050cd26236866753cb869ef69984da6bd
nakama55 <nakamakikun55@gmail.com>
2023-12-03 06:28:05 +0700

    Calculator-with-JS
    Test-Run-1

---
+++ script.js
@@ -0,0 +1,31 @@
+// main calculator
+const Container = document.querySelector('.container');
+
+Container.addEventListener('click', function(e) {
+    const clickb = e.target;
+    const resultPlace = document.getElementById('ResultPlace');
+    const buttonValue = clickb.value;
+    if (clickb.tagName === 'INPUT') {
+        if (clickb.id !== 'ResultPlace') {
+            if (clickb.value === 'CA') {
+                resultPlace.value = '';
+            }else if (clickb.value === 'DE') {
+                resultPlace.value = resultPlace.value.slice(0, -1);
+            }else if (clickb.value === '=') {
+                resultPlace.value = eval(resultPlace.value);
+            }else  {
+                resultPlace.value += buttonValue;
+            }
+        }
+    }
+})
+
+// main gallery
+
+const backgroundGallery = document.querySelector('.backgroundGallery');
+
+backgroundGallery.addEventListener('click', function(e) {
+    if (e.class !== 'backgroundGallery') {
+        Container.style.backgroundImage = "url('" + e.target.src + "')";
+    }
+})
